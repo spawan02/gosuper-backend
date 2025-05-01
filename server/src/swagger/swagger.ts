3ff2c85795c246fe79dea965381f2d5a15b24c87
pawan <spavandsp@gmail.com>
2025-05-01 21:55:22 +0530

    fix: swagger url

--- server/src/swagger/swagger.ts
+++ server/src/swagger/swagger.ts
@@ -16,7 +16,7 @@ const swaggerOptions = {
     },
     servers: [
       {
-        url: 'http://localhost/3000',
+        url: 'https://go.itsarc.me/',
         description: 'Production server on Railway',
       },
     ],
